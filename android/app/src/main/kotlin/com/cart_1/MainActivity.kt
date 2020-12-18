4ef071b8ca94107972e8732c23cd874d57e86c6d
abdoo <kankro766@gmail.com>
2020-12-18 17:06:06 +0300

    Add 1

---
+++ android/app/src/main/kotlin/com/cart_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cart_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/bloc/cartListBloc.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cart_1/bloc/provider.dart';
+import 'package:cart_1/model/foodItem.dart';
+import 'package:rxdart/rxdart.dart';
+
+class CartListBloc extends BlocBase{
+
+  CartListBloc();
+
+  var _listControler = BehaviorSubject<List<FoodItem>>.seeded([]);
+
+  CartProvider  provider = CartProvider();
+  //output
+  Stream<List<FoodItem>> get ListStream => _listControler.stream;
+
+  //input
+  Sink<List<FoodItem>> get ListSink => _listControler.sink;
+
+  addToList(FoodItem foodItem) {
+    ListSink.add(provider.addToList(foodItem));
+  }
+
+  removeFromList (FoodItem foodItem){
+    ListSink.add(provider.removeFromList(foodItem));
+  }
+
+  @override
+  void dispose() {// will be called automatically
+    _listControler.close();
+    super.dispose();
+  }
+}
